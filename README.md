4455e1bb1217b93900344d0da8f1cab5b9e3f1b5
festeban <festeban@medallia.com>
2019-12-27 20:39:28 -0300

    Add models and gitignore

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ sudo -u postgres psql
 create database ludo;
 create user ludo with encrypted password 'ludo';
 grant all privileges on database ludo to ludo;
+
+## Migrate
+- Change your models (in models.py).
+- Run python3 manage.py makemigrations to create migrations for those changes
+- Run python3 manage.py migrate to apply those changes to the database.
+
+# Things to check up eventually
+- https://django-localflavor.readthedocs.io/en/latest/
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    padron = models.CharField(max_length=50)
+    email = models.EmailField()
+    status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Subject (models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Professor (models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Course (models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Final(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FinalExam (models.Model):
+    final = models.ForeignKey(Final, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
