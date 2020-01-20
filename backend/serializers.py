c03e47786c107445b116e0bd1361491f9898d1d9
festeban <festeban@medallia.com>
2020-01-19 21:11:01 -0300

    Removed wrong import

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework_json_api import serializers
+from rest_framework import serializers
 from .models import *
+
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
