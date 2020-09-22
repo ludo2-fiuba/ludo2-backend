9b8647c8bac026c824b1a227eecfc77fde191a9b
Federico Esteban <festeban@medallia.com>
2020-09-22 20:52:45 -0300

    Fix on serializerg

--- backend/serializers/user_serializer.py
+++ backend/serializers/user_serializer.py
@@ -6,7 +6,7 @@ from backend.interactors.image_validator_interactor import ImageValidatorInterac
 
 class UserCustomCreateSerializer(UserCreateSerializer):
     def create(self, validated_data):
-        b64_string = self.context['request']['image']
+        b64_string = self.context['request'].data['image']
         validation_result = ImageValidatorInteractor(b64_string).validate_image()
         if validation_result.errors:
             raise serializers.ValidationError(validation_result.errors)
