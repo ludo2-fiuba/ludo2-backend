0c26da7da0cf51c8b287840ea36df646c62c2bc1
Federico Esteban <festeban@medallia.com>
2020-10-15 21:49:38 -0300

    add new app ludo-backend

--- backend/urls.py
+++ backend/urls.py
@@ -18,13 +18,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
 
     # path to djoser end points
-    # path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 
     path('', include(auth_router.urls)),
-
-    # swagger endpoints
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
