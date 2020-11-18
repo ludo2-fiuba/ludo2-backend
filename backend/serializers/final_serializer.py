cf026e3b7503963533f96179fb8937c4f4b1f9fd
Federico Esteban <festeban@medallia.com>
2020-11-17 22:10:06 -0300

    Add siu_id to serializers

--- backend/serializers/final_serializer.py
+++ backend/serializers/final_serializer.py
@@ -18,11 +18,11 @@ class FinalTeacherSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Final
-        fields = ('id', 'subject', 'date', 'qrid', 'status', 'final_exams')
+        fields = ('id', 'subject', 'date', 'qrid', 'status', 'siu_id', 'final_exams')
 
 
 class FinalTeacherListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Final
-        fields = ('id', 'subject', 'date', 'status')
+        fields = ('id', 'subject', 'date', 'status', 'siu_id')
 
