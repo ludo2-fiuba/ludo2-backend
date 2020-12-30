ae707e68f8f6b5b6b6e94bd09c551dd9616578e1
Federico Esteban <festeban@medallia.com>
2020-12-30 00:24:01 -0300

    Readd external_mapper after wrongfully deleting it

---
+++ backend/external_mappers/external_mapper.py
@@ -0,0 +1,24 @@
+class ExternalMapper:
+    MAPPINGS = {
+        'id': 'id', 'codigo': 'code', 'nombre': 'name', 'correlativas': 'correlatives',
+        'materiaId': 'subject_id', 'docenteId': 'teacher_id', 'materia': 'subject'
+    }
+
+    def map(self, data):
+        if type(data) is list:
+            return [self._map(el) for el in data]
+        elif type(data) is dict:
+            return self._map(data)
+        else:
+            return data
+
+    def _map(self, data):
+        res = {}
+        for key, val in data.items():
+            if type(val) is list:
+                res[self.MAPPINGS[key]] = [self.map(el) for el in val]
+            elif type(val) is dict:
+                res[self.MAPPINGS[key]] = self.map(val)
+            else:
+                res[self.MAPPINGS[key]] = val
+        return res
--- backend/external_mappers/subject_mapper.py
+++
@@ -1,7 +0,0 @@
-from backend.external_mappers.external_mapper import ExternalMapper
-
-
-class SubjectMapper(ExternalMapper):
-    MAPPINGS = {
-        'id': 'id', 'codigo': 'code', 'nombre': 'name', 'correlativas': 'correlatives'
-    }
