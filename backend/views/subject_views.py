e40382f5ff27b96e4361f0de2c0978f690cb5154
Federico Esteban <festeban@medallia.com>
2021-05-12 21:07:28 -0300

    Add endpoint to get subject history for a stdent

--- backend/views/subject_views.py
+++ backend/views/subject_views.py
@@ -1,13 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+from backend.models import FinalExam
+from backend.permissions import IsStudent
+from backend.serializers.final_exam_serializer import FinalExamSerializer
 from backend.services.siu_service import SiuService
 from backend.views.utils import respond_plain
 
 
 class SubjectViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsStudent]
+    serializer_class = FinalExamSerializer
+    queryset = FinalExam.objects.all()
+
+    @action(detail=False, methods=["GET"])
+    def history(self, request):
+        result = self.get_queryset().filter(final__subject_siu_id=request.query_params['subject_siu_id'], student=request.user.id).order_by('final__date')
+        return Response(FinalExamSerializer(result, many=True).data, status.HTTP_200_OK)
 
     def list(self, request):
         result = SiuService().list_subjects()
