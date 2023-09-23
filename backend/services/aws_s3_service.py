65d9c746b227476e809a31ed652b5a21c510c92c
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2023-09-23 18:49:45 -0300

    Add AWS domain config

--- backend/services/aws_s3_service.py
+++ backend/services/aws_s3_service.py
@@ -14,13 +14,14 @@ class AwsS3Service:
             aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
         )
         self.bucket = os.environ["AWS_BUCKET_NAME"]
+        self.s3_api_url = os.getenv("AWS_DOMAIN_URL", "s3.amazonaws.com")
 
     def upload_b64_image(self, b64_string, file_name):
         return self.upload_object(io.BytesIO(decode_image(b64_string)), file_name)
 
     def upload_object(self, generic_object, file_name):
         self.client.upload_fileobj(generic_object, self.bucket, file_name, ExtraArgs={'ACL': 'public-read'})
-        return f"https://{self.bucket}.s3.amazonaws.com/{file_name}"
+        return f"https://{self.bucket}.{self.s3_api_url}/{file_name}"
 
     def download_object(self, file_name):
         return self.client.get_object(Bucket=self.bucket, Key=file_name)['Body']
