b368c5f0809f6f304e811f91764b6153fe10c30a
Martin <martincohen98@gmail.com>
2023-10-21 14:12:03 -0300

    Correlatives endpoint

--- backend/views/subject_views.py
+++ backend/views/subject_views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from backend.api_exceptions import InvalidSubjectCodeError
 from backend.models import FinalExam
 from backend.permissions import IsStudent
 from backend.serializers.final_exam_serializer import FinalExamSerializer
@@ -19,6 +20,18 @@ class SubjectViewSet(viewsets.ModelViewSet):
     def history(self, request):
         result = self.get_queryset().filter(final__subject_siu_id=request.query_params['subject_siu_id'], student=request.user.id).order_by('final__date')
         return Response(FinalExamSerializer(result, many=True).data, status.HTTP_200_OK)
+
+    @action(detail=False, methods=["GET"])
+    def correlatives(self, request):
+        self.extra = {}
+        id = request.query_params.get('id')
+        response = SiuService().list_subjects({'id': id})
+
+        if not response:
+            raise InvalidSubjectCodeError()
+
+        result = SiuService().correlative_subjects(response[0])
+        return respond_plain(result)
 
     def list(self, request):
         result = SiuService().list_subjects()
