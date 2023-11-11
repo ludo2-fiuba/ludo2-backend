ef0cc349747dcfe242cc6105a175a8951bf3d307
Martin <martincohen98@gmail.com>
2023-11-11 16:19:28 -0300

    Added evaluations to semester

--- backend/serializers/evaluation_serializer.py
+++ backend/serializers/evaluation_serializer.py
@@ -2,16 +2,13 @@ from rest_framework import serializers
 
 from backend.models import Evaluation
 
-from .semester_serializer import SemesterSerializer
-
 
 class EvaluationSerializer(serializers.ModelSerializer):
     evaluation_name = serializers.CharField()
-    semester = SemesterSerializer()
     passing_grade = serializers.IntegerField()
     start_date = serializers.DateTimeField()
     end_date = serializers.DateTimeField()
 
     class Meta:
         model = Evaluation
-        fields = ('evaluation_name', 'semester', 'passing_grade', 'start_date', 'end_date')
+        fields = ('evaluation_name', 'passing_grade', 'start_date', 'end_date')
--- backend/serializers/semester_serializer.py
+++ backend/serializers/semester_serializer.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from backend.models import Semester
 
 from .commission_serializer import CommissionSerializer
+from .evaluation_serializer import EvaluationSerializer
 
 
 class SemesterSerializer(serializers.ModelSerializer):
     year_moment = serializers.CharField()
     start_date = serializers.DateTimeField()
     commission = CommissionSerializer()
+    evaluations = EvaluationSerializer(many=True)
 
     class Meta:
         model = Semester
-        fields = ('year_moment', 'start_date', 'commission')
+        fields = ('year_moment', 'start_date', 'commission', 'evaluations')
