8b83dcd7e59f29ab312dcc41b6a9c8348f158b96
Martin <martincohen98@gmail.com>
2023-12-02 16:50:49 -0300

    Added students tu semester serializer

--- backend/serializers/semester_serializer.py
+++ backend/serializers/semester_serializer.py
@@ -4,6 +4,7 @@ from backend.models import Semester
 
 from .commission_serializer import CommissionSerializer
 from .evaluation_serializer import EvaluationSerializer
+from .student_serializer import StudentSerializer
 
 
 class SemesterSerializer(serializers.ModelSerializer):
@@ -11,7 +12,8 @@ class SemesterSerializer(serializers.ModelSerializer):
     start_date = serializers.DateTimeField()
     commission = CommissionSerializer()
     evaluations = EvaluationSerializer(many=True)
+    students = StudentSerializer(many=True)
 
     class Meta:
         model = Semester
-        fields = ('id', 'year_moment', 'start_date', 'commission', 'evaluations')
+        fields = ('id', 'year_moment', 'start_date', 'commission', 'evaluations', 'students')
