d919657f100db5d5a814045c6f3a4fbede596ee3
Martin <martincohen98@gmail.com>
2024-04-20 18:30:10 -0300

    Fix

--- backend/models/teacher_role.py
+++ backend/models/teacher_role.py
@@ -8,7 +8,7 @@ class TeacherRole(models.Model):
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE, verbose_name="Profesores", related_name='teacher_roles')
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name="Semestres")
     grader_weight = models.FloatField(default=1.0, verbose_name="Peso al asignar correctores")
-    role = models.CharField(max_length=25, null=False, verbose_name="Rol")
+    role = models.CharField(max_length=30, null=False, verbose_name="Rol")
 
     class Meta:
         verbose_name = "Rol de Profesor"
