8b26787f363a5aeb9758ce13b9b1a9d60c6c3268
Martin <martincohen98@gmail.com>
2024-04-27 18:09:20 -0300

    Added evaluation put and notification on end date change

--- backend/services/notification_service.py
+++ backend/services/notification_service.py
@@ -11,6 +11,9 @@ class NotificationService:
     def notify_final_grade(self, final):
         self.notify_devices(final.final_exams.filter(grade__isnull=False), f"El docente ya subió tu nota para tu final de {final.subject_name} del día {final.date.date()}")
 
+    def notify_evaluation_date(self, evaluation):
+        self.notify_student_devices(evaluation.semester.students, f"El la fecha de {evaluation} ha sido cambiada")
+
     def notify_evaluation_grade(self, evaluation):
         self.notify_devices(evaluation.submissions.filter(grade__isnull=False), f"El docente de {evaluation.semester.commission.subject_name} ya subió tu nota de {evaluation.evaluation_name}")
 
@@ -20,5 +23,11 @@ class NotificationService:
     def notify_devices(self, final_exams, message):
         for fe in final_exams:
             devices = GCMDevice.objects.filter(user=fe.student.user, active=True)
+            for device in devices:
+                device.send_message(message)
+
+    def notify_student_devices(self, students, message):
+        for student in students:
+            devices = GCMDevice.objects.filter(user=student.user, active=True)
             for device in devices:
                 device.send_message(message)
--- backend/views/evaluation_teacher_views.py
+++ backend/views/evaluation_teacher_views.py
@@ -6,9 +6,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from backend.models import Evaluation, Semester
-from backend.serializers.evaluation_serializer import EvaluationPostSerializer
+from backend.serializers.evaluation_serializer import (
+    EvaluationPostSerializer, EvaluationSerializer)
 from backend.services.notification_service import NotificationService
 from backend.views.base_view import BaseViewSet
+from backend.views.utils import datetime_format
 
 
 class EvaluationTeacherViewSet(BaseViewSet):
@@ -30,6 +32,52 @@ class EvaluationTeacherViewSet(BaseViewSet):
                 evaluation_serializer.save()
                 return Response(evaluation_serializer.data, status=status.HTTP_201_CREATED) 
         return Response(evaluation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def _semester(self, semester_pk):
+        return get_object_or_404(Semester.objects, id=semester_pk)
+    
+    @action(detail=False, methods=['PUT'])
+    @swagger_auto_schema(
+        tags=["Evaluations"],
+        operation_summary="Adds an evaluation for a semester"
+    )
+    def update_evaluation(self, request):
+        evaluation_data = request.data
+        semester = self._semester(evaluation_data["semester_id"])
+
+        if semester.commission.chief_teacher != request.user.teacher:
+            return Response("Teacher not chief teacher in commission", status=status.HTTP_403_FORBIDDEN)
+
+        evaluations = Evaluation.objects.filter(semester=semester).all()
+
+        evaluation = None
+        for an_evaluation in evaluations:
+            if an_evaluation.evaluation_name == evaluation_data["evaluation_name"]:
+                evaluation = an_evaluation
+
+        if not evaluation:
+            return Response("Evaluation does not exist", status=status.HTTP_404_NOT_FOUND)
+        
+        if evaluation_data["passing_grade"]:
+            evaluation.passing_grade = evaluation_data["passing_grade"]
+        
+        if evaluation_data["is_graded"] is not None:
+            evaluation.is_graded = evaluation_data["is_graded"]
+        
+        start_date = datetime_format(evaluation_data["start_date"])
+        if start_date:
+            evaluation.start_date = start_date
+        
+        end_date = datetime_format(evaluation_data["end_date"])
+        if end_date:
+            evaluation.end_date = end_date
+
+        if evaluation.start_date > evaluation.end_date:
+            return Response("Start date cannot be after end date", status=status.HTTP_403_FORBIDDEN)
+
+        evaluation.save()
+
+        return Response(EvaluationSerializer(evaluation).data, status=status.HTTP_400_BAD_REQUEST)
 
     def _semester(self, semester_pk):
         return get_object_or_404(Semester.objects, id=semester_pk)
