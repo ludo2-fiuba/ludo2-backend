e893985797cc0b788ceb776fb21a8c50a1de408e
Martin <martincohen98@gmail.com>
2024-05-04 16:41:06 -0300

    Added commission semesters endpoint

--- backend/views/commission_views.py
+++ backend/views/commission_views.py
@@ -2,12 +2,13 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from backend.models import Commission, TeacherRole
+from backend.models import Commission, Semester
 from backend.serializers.commission_serializer import CommissionSerializer
-from backend.serializers.teacher_role_serializer import TeacherRoleSerializer
+from backend.serializers.semester_serializer import SemesterSerializer
 from backend.views.base_view import BaseViewSet
 
 
@@ -40,4 +41,17 @@ class CommissionViewSet(BaseViewSet):
     def subject_commissions(self, request):
         result = self.get_queryset().filter(subject_siu_id=request.query_params['subject_siu_id'])
         return Response(self.get_serializer(result, many=True).data, status.HTTP_200_OK)
+    
+    @action(detail=True, methods=["GET"])
+    @swagger_auto_schema(
+        tags=["Commissions"],
+        operation_summary="Gets all commissions for a subject",
+        manual_parameters=[
+            openapi.Parameter('subject_siu_id', openapi.IN_QUERY, description="Id of subject to get commission from", type=openapi.FORMAT_INT64)
+        ]
+    )
+    def semesters(self, request, pk=None):
+        commission = get_object_or_404(self.queryset, id=pk)
+        result = Semester.objects.get_queryset().filter(commission=commission)
+        return Response(SemesterSerializer(result, many=True).data, status.HTTP_200_OK)
 
