abd08e56ceae3f1fa1439501a1c671812359d473
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2024-06-27 00:11:02 -0300

    aws_s3_service: Add support for OCI buckets :)

--- backend/services/aws_s3_service.py
+++ backend/services/aws_s3_service.py
@@ -11,17 +11,18 @@ class AwsS3Service:
         self.client = boto3.client(
             's3',
             aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+            endpoint_url=os.getenv("AWS_ENDPOINT_URL")
         )
         self.bucket = os.environ["AWS_BUCKET_NAME"]
-        self.s3_api_url = os.getenv("AWS_DOMAIN_URL", "s3.amazonaws.com")
+        self.public_read_domain = os.getenv("AWS_PUBLIC_READ_DOMAIN", f"{self.bucket}.s3.amazonaws.com")
 
     def upload_b64_image(self, b64_string, file_name):
         return self.upload_object(io.BytesIO(decode_image(b64_string)), file_name)
 
     def upload_object(self, generic_object, file_name):
         self.client.upload_fileobj(generic_object, self.bucket, file_name)
-        return f"https://{self.bucket}.{self.s3_api_url}/{file_name}"
+        return f"https://{self.public_read_domain}/{file_name}"
 
     def download_object(self, file_name):
         return self.client.get_object(Bucket=self.bucket, Key=file_name)['Body']
